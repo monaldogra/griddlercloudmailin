7971e634dc32cb42f7d2013df8658680af21c63b
monaldogra75@gmail.com <monal@maropost.com>
2018-01-31 19:03:26 +0530

    first commit

---
+++ app/models/email_processor.rb
@@ -0,0 +1,35 @@
+class EmailProcessor
+
+
+  def initialize(email)
+   @email = email
+  end
+
+
+  # def self.process(email)
+  #   Post.create!({ body: email.body, email: email.from })
+  # end
+
+
+  def process
+
+   # all of your application-specific code here - creating models,
+   # processing reports, etc
+
+   # here's an example of model creation
+
+
+    Post.create!({ body: email.body, email: email.from })
+
+
+   # user = User.find_by_email(@email.from[:email])
+   # user.posts.create!(
+   #   subject: @email.subject,
+   #   body: @email.body
+   # )
+
+
+ end
+
+
+end
