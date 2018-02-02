c4183f67b8e12fbcc9484ce60a0dc3147812f303
monaldogra75@gmail.com <monal@maropost.com>
2018-02-02 20:12:28 +0530

    fourth commit

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -4,5 +4,6 @@ class IncomingMailsController < ApplicationController
   def create
     Rails.logger.debug params.inspect
     Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
+    Post.create!({ body: params[:plain], email: params[:headers][:from] })
   end
 end
